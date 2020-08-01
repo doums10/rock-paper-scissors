84941d45487e8eefab0fb3d62a7d6259abbcc5fa
mamadou sylla <m27sylla@gmail.com>
2020-08-01 14:03:38 +0200

    randomisation du choix de l'ordinateur via fonction

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ const rock_div = document.getElementById('r');
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-rock_div.addEventListener('click', function() {
-	console.log("hey you clicked on rock");
-});
+function getComputerChoice()
+{
+	const choices = ['r', 'p', 's'];
+	console.log(Math.random() * 3);
+}
+getComputerChoice();
+function game(userChoice)
+{
+	console.log("🔥🔥🔥    " + userChoice);
+}
+
+function main()
+{
+	rock_div.addEventListener('click', function ()
+	{
+		game("r");
+	});
+	paper_div.addEventListener('click', function ()
+	{
+		game("p");
+	});
+	scissors_div.addEventListener('click', function ()
+	{
+		game("s");
+	});
+}
+main();
+
+
